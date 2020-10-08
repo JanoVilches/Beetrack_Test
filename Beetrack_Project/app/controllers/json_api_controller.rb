dedf16692add33ec71500b9f96070a50c8340415
JanoVilches <alejandro.vilches@sansano.usm.cl>
2020-10-08 12:55:30 -0300

    se crea la lógica para update or create al momento de recibir la información de una nueva posición del vehiculo

--- Beetrack_Project/app/controllers/json_api_controller.rb
+++ Beetrack_Project/app/controllers/json_api_controller.rb
@@ -4,6 +4,37 @@ class JsonApiController < ApplicationController
 
   def new_waypoint
     puts params[:latitude], params[:longitude], params[:sent_at], params[:vehicle_identifier]
+    
+    v = Vehicle.where(idVehicle: params[:vehicle_identifier])
+    puts v
+    
+    if v != []
+      puts "modificando posicion de vehiculo"
+      
+      loc = Location.find_by(vehicle_id: v[0].id, is_actual: 1)
+      loc.update(is_actual: 0)
+
+      loc = Location.create(
+            latitude: params[:latitude], 
+            longitude: params[:longitude],
+            sent_at: params[:sent_at],
+            vehicle_id: v[0].id,
+            is_actual: 1,
+      )
+    
+    else
+      puts "añadiendo nuevo vehiculo"
+      vehicle = Vehicle.create(idVehicle: params[:vehicle_identifier])
+
+      loc = Location.create(
+                    latitude: params[:latitude], 
+                    longitude: params[:longitude],
+                    sent_at: params[:sent_at],
+                    vehicle_id: vehicle.id,
+                    is_actual: 1,
+                  )
+    end
+
 
   end
 
